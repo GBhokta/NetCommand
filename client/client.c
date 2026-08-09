6d3a7c0b3596f0eb95aa00f15a1dde6871f49ac7
George Bhokta <georgebhokta@gmail.com>
2026-08-09 17:50:36 +0530

    communication from both sides

--- client/client.c
+++ client/client.c
@@ -9,58 +9,61 @@
 
 int main()
 {
-
     int sock_fd;
-
     struct sockaddr_in server_addr;
 
-    sock_fd = socket(AF_INET,SOCK_STREAM,0);
-
-    if(sock_fd<0)
-    {
+    sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_fd < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
     printf("Socket created successfully\n");
 
-    server_addr.sin_family = AF_INET; //for IPv4
-    server_addr.sin_port = htons(PORT); //convert port number to network byte order
-     if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0)
-    {
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
         perror("inet_pton");
         close(sock_fd);
         return 1;
     }
-    
 
-    int temp;
-    temp = connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (temp < 0)
-    {
+    if (connect(sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("connect failed");
         close(sock_fd);
         return 1;
     }
+    
+    printf("Connected to server %s:%d\n", SERVER_IP, PORT);  // MOVED HERE
 
-    char message[1024];
+    char ReadBuffer[1024];
+    char WriteBuffer[1024];
 
-    while (1)
-    {
-        printf("Enter message to send to server: ");
-        if (fgets(message, sizeof(message), stdin) == NULL)
+    while (1) {
+        printf("Client: ");
+        if (fgets(WriteBuffer, sizeof(WriteBuffer), stdin) == NULL)
+            continue;
+            
+        // Check for exit from client side
+        if (strcmp(WriteBuffer, "exit\n") == 0) {
+            send(sock_fd, WriteBuffer, strlen(WriteBuffer), 0);
             break;
+        }
+        
+        send(sock_fd, WriteBuffer, strlen(WriteBuffer), 0);
 
-        send(sock_fd, message, strlen(message), 0);
-
-        printf("Message sent to server: %s", message);
-        if (strcmp(message, "exit\n") == 0)
-        {
+        int bytes_received = recv(sock_fd, ReadBuffer, sizeof(ReadBuffer) - 1, 0);
+        if (bytes_received > 0) {
+            ReadBuffer[bytes_received] = '\0';
+            printf("Server: %s", ReadBuffer);
+        } else if (bytes_received == 0) {
+            printf("Server disconnected\n");
+            break;
+        } else {
+            perror("recv");
             break;
         }
     }
 
-    printf("Connected to server %s:%d\n", SERVER_IP, PORT);
     close(sock_fd);
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -10,84 +9,81 @@
 
 int main()
 {
-    int server_fd; // server listening socket file descriptor
-    int client_fd; // client socket
-
+    int server_fd, client_fd;
     struct sockaddr_in server_addr, client_addr;
-
-    socklen_t client_len = sizeof(client_addr); // size of cleint address
+    socklen_t client_len = sizeof(client_addr);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server_fd < 0)
-    {
+    if (server_fd < 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;         // for IPv4
-    server_addr.sin_addr.s_addr = INADDR_ANY; // accept connections from any IP address
-    server_addr.sin_port = htons(PORT);       // convert port number to network byte order
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-    // 3. Bind socket to IP and port
-    if (bind(server_fd,
-             (struct sockaddr *)&server_addr,
-             sizeof(server_addr)) < 0)
-    {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("bind");
         close(server_fd);
         return 1;
     }
-
     printf("Server bound to port %d\n", PORT);
 
-    // 4. Start listening
-    if (listen(server_fd, 5) < 0)
-    {
+    if (listen(server_fd, 5) < 0) {
         perror("listen");
         close(server_fd);
         return 1;
     }
-
     printf("Server listening...\n");
 
-    // 5. Accept a client
-    client_fd = accept(server_fd,
-                       (struct sockaddr *)&client_addr,
-                       &client_len);
-
-    if (client_fd < 0)
-    {
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd < 0) {
         perror("accept");
         close(server_fd);
         return 1;
     }
-
-    char buffer[1024];
-
-    int bytes_received;
-    while (1)
-    {
-        bytes_received = recv(
-            client_fd,
-            buffer,
-            sizeof(buffer) - 1,
-            0);
-
-        if (bytes_received > 0)
-        {
-            buffer[bytes_received] = '\0';
-            printf("Client says: %s", buffer);
-        }
-        else if(strcmp(buffer,"exit\n")==0)
-        {
-            break;
-        }
-    }
-
     printf("Client connected\n");
 
-    close(server_fd);
+    char Readbuffer[1024];
+    char Writebuffer[1024];
+    int bytes_received;
 
+    while (1) {
+        bytes_received = recv(client_fd, Readbuffer, sizeof(Readbuffer) - 1, 0);
+        
+        if (bytes_received > 0) {
+            Readbuffer[bytes_received] = '\0';
+            printf("Client: %s", Readbuffer);
+            
+            // Check for exit command from client
+            if (strcmp(Readbuffer, "exit\n") == 0) {
+                printf("Client requested exit\n");
+                break;
+            }
+        } else if (bytes_received == 0) {
+            printf("Client disconnected\n");
+            break;
+        } else {
+            perror("recv");
+            break;
+        }
+
+        // Get server response
+        printf("Server: ");
+        if (fgets(Writebuffer, sizeof(Writebuffer), stdin) == NULL)
+            break;
+            
+        // Check for server exit
+        if (strcmp(Writebuffer, "exit\n") == 0) {
+            send(client_fd, Writebuffer, strlen(Writebuffer), 0);
+            break;
+        }
+        
+        send(client_fd, Writebuffer, strlen(Writebuffer), 0);
+    }
+
+    close(client_fd);
+    close(server_fd);
     return 0;
 }
